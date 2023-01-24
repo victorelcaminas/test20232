9c2d02940f50332fe0d1ab8c81a28cd158738d96
Victor Alonso <victor@ieselcaminas.org>
2023-01-24 13:21:39 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/test20232/Test20232.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.test20232;
+
+/**
+ *
+ * @author victoralonso
+ */
+public class Test20232 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
